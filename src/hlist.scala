a06f82450cfb2186560662eba4e59de3530579e7
Jon Pretty <jon.pretty@propensive.com>
2020-12-21 17:26:58 +0100

    Advanced Day 1

--- src/hlist.scala
+++ src/hlist.scala
@@ -1,5 +1,24 @@
 package hlists
 
-trait HList[+T <: HList[_]] { def :+:[V](value: V) = HCons(value, this) }
-case class HCons[H, +T <: HList[_]](head: H, tail: T) extends HList[HCons[H, T]]
-case object HNil extends HList[Nothing]
+sealed trait HList[T <: HList[_]] {
+  def :+:[V](value: V) = new HCons(value, this)
+  def length: Int
+  type Intersection
+}
+
+class HCons[H, T <: HList[_]](head: H, val tail: T) extends HList[HCons[H, T]] {
+  def length = tail.length + 1
+  type Intersection = H with tail.Intersection
+}
+
+object HNil extends HList[Nothing] {
+  def length = 0
+  type Intersection = Any
+}
+
+object Test extends App {
+
+  val myHlist = 'z' :+: "x" :+: 4 :+: HNil
+  println(myHlist.length)
+
+}
